0f187e86d1734439cc7c1c20ec33b47fb0fa2975
YC57384-cmyk <YC57384@UM.EDU.MO>
2026-03-10 15:33:56 +0800

    Update data_simulation.R

--- data_simulation.R
+++ data_simulation.R
@@ -1,1 +1,5 @@
-
+if(!require(lavaan)) install.packages("lavaan")
+library(lavaan)
+# Measurement Model (Defining Latent Variables based on established scales)
+  # SAQ = Sexual Assertiveness (3 indicator items for simplicity)
+  Assertiveness =~ 0.8*SAQ1 + 0.7*SAQ2 + 0.8*SAQ3
